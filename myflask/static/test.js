64f3e45201d0e5d8a3e0aac0527bf56e25525c01
Suhas Bk <kowligi1998@gmail.com>
2019-03-19 20:58:24 +0530

    vue!!!

--- myflask/static/test.js
+++ myflask/static/test.js
@@ -1,12 +1,19 @@
 var msg = new Vue({
     el: '#msg',
     data: {
-        message: 'Hello Vue!'
+        message: 'How many do you want buy?',
+        count: 0
     },
     methods:{
       click:function(){
-        alert('clicked!!!')
-      }
+        msg.count+=1
+        document.getElementById("left").style.backgroundColor-=1;
+        if(msg.count>5)
+        {
+            document.getElementById("msg").innerHTML="<h1>Gone Bro!!</h1>";
+            document.getElementById("msg").style.backgroundColor = "red";
+        }
+    },
     },
     delimiters: ['[[', ']]']
 });
