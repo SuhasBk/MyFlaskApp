497a9a3497243876aad8406495d327ee9b6d5bcd
Suhas B Kowligi <kowligi1998@gmail.com>
2019-06-29 10:57:39 +0530

    deployed

--- portssh.py
+++ portssh.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 import os
 from subprocess import call
-import webbrowser
 call("ssh -R hyperbyte:80:localhost:5000 serveo.net",shell=True)
--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 from myflask import socketio,app
-
 if __name__=='__main__':
     socketio.run(app,host='localhost',debug=True)
