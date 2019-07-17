c3369abf7e3e38b59112ba74b038c8e2e8651e3f
Suhas B Kowligi <kowligi1998@gmail.com>
2019-07-17 22:25:09 +0530

    bugs

--- cric.py
+++ cric.py
@@ -6,7 +6,7 @@ base_url = "https://www.cricbuzz.com"
 headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0'}
 
 r = requests.get(base_url+'/cricket-match/live-scores',headers=headers)
-
+print(r.text)
 s = BeautifulSoup(r.text,'html.parser')
 
 try:
@@ -14,6 +14,6 @@ try:
 except AttributeError:
     print('No LIVE cricket matches happening')
     exit()
-print(str(len(live)))
+
 for i,j in enumerate(live,1):
     print(i,' - ',j.text.lstrip())
