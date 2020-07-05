6e4782648d12d9a2a6a4cbbb63d4cd06d6a927aa
Suhas BK <kowligi1998@gmail.com>
2020-07-05 16:38:51 +0530

    trying heroku selenium configuration

--- deccan.py
+++ deccan.py
@@ -32,7 +32,15 @@ class Deccan:
         op = FirefoxOptions()
         op.headless = True
 
-        self.browser = webdriver.Firefox(options=op,firefox_profile=fp,service_log_path="/dev/null")
+        # heroku selenium configurations:
+        gecko_cloud_path = os.environ.get('GECKODRIVER_PATH',None)
+        firefox_cloud_path = os.environ.get('FIREFOX_BIN', None)
+
+        if gecko_cloud_path and firefox_cloud_path:
+            self.browser = webdriver.Firefox(executable_path=gecko_cloud_path,options=op,firefox_profile=fp,service_log_path="/dev/null",firefox_binary=firefox_cloud_path)
+        else:
+            print("Running on local machine... phew..")
+            self.browser = webdriver.Firefox(options=op,firefox_profile=fp,service_log_path="/dev/null")
 
         self.merger = PdfFileMerger()
         self.order = []
