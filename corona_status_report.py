e0484e084ba5b273fa488048891459d4f0f350a7
Suhas Kowligi <kowligi1998@gmail.com>
2020-08-22 19:49:52 +0530

    updates to WHO corona status reports, now weekly

--- corona_status_report.py
+++ corona_status_report.py
@@ -3,12 +3,15 @@
 from requests import Session
 from bs4 import BeautifulSoup
 from datetime import date
+import re
 
 s = Session()
-r = s.get("https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports")
+r = s.get("http://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports")
 html = BeautifulSoup(r.text,'html.parser')
-pdf_url = html.findAll('div',{'class':'sf-content-block'})[10].find('a').get('href')
+
+pdf_url = html.find('a', text=re.compile(r'Update - \d+')).get('href')
 full_url = "https://" + r.url.split('/')[2] + pdf_url
+
 p = s.get(full_url)
 open(f"myflask/static/report.pdf","wb+").write(p.content)
 s.close()
